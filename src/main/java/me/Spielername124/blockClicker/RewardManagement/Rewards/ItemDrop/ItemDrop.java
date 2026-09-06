f83b79b0da76ba0a9d6ac818062a16c30cd5b223
Spielername124 <237307027+Spielername124@users.noreply.github.com>
2026-09-06 14:00:37 +0200

    fixed that depositing to inventory first tries to deposit to the offhand

--- src/main/java/me/Spielername124/blockClicker/RewardManagement/Rewards/ItemDrop/ItemDrop.java
+++ src/main/java/me/Spielername124/blockClicker/RewardManagement/Rewards/ItemDrop/ItemDrop.java
@@ -12,6 +12,7 @@ import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 import java.util.Map;
 
@@ -32,24 +33,41 @@ public class ItemDrop extends Reward {
         droppedItem = DroppedItem.create(plugin, rewardData);
     }
 
-    protected void execute(Player player, Location location, GlobalFlags flags, RewardSoundAndParticle sound, ItemStack toolUsed, Block block, EventWideFlags eventWideFlags){
+    protected void execute(Player player, Location location, GlobalFlags flags, RewardSoundAndParticle sound, ItemStack toolUsed, Block block, EventWideFlags eventWideFlags) {
 
         //handle the Special case that XP should be dropped
-        if(itemName.equals("xp")){
+        if (itemName.equals("xp")) {
             XpDrop.performXpDrop(flags, player, location, amount.getAmount(), toolUsed);
             return;
         }
 
         ItemStack reward = droppedItem.getItem();
 
-        if (reward != null) {
-            if (flags.depositToInventory){
+        if (reward != null && reward.getAmount() >= 0) {
+            if (flags.depositToInventory) {
                 if (player != null)
-                    player.getInventory().addItem(reward);
-            }
+                    performDepositToInventory(player, reward);
+            } else location.getWorld().dropItemNaturally(location, reward);
+        }
+    }
 
-            else location.getWorld().dropItemNaturally(location, reward);
+
+    // handles the depositing directly to the inventory.
+    private void performDepositToInventory(Player player, ItemStack item) {
+        PlayerInventory inventory = player.getInventory();
+        ItemStack offHand = inventory.getItemInOffHand();
+
+        if (offHand.isSimilar(item)) {
+            int spaceLeft = offHand.getMaxStackSize() - offHand.getAmount();
+            if (spaceLeft > 0) {
+                int toAdd = Math.min(spaceLeft, item.getAmount());
+                offHand.setAmount(offHand.getAmount() + toAdd);
+                item.setAmount(item.getAmount() - toAdd);
+            }
+        }
+
+        if (item.getAmount() > 0) {
+            inventory.addItem(item);
         }
     }
 }
-
